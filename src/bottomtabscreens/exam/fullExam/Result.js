a0ef58fd5095fe1907da01649e91f7b5be9ef325
parthdevwork <parth.devwork18@gmail.com>
2024-07-04 10:54:13 +0530

    Some changes

--- src/bottomtabscreens/exam/fullExam/Result.js
+++ src/bottomtabscreens/exam/fullExam/Result.js
@@ -65,54 +65,6 @@ const Result = () => {
   };
   let queData = JSON.stringify(JSON.stringify(filterQueAns));
 
-  // useEffect(() => {
-  //   if (queData) {
-  //     let data = JSON.stringify({
-  //       model: 'gpt-3.5-turbo-1106',
-  //       messages: [
-  //         {
-  //           role: 'user',
-  //           content: `based on given questions and answers i want to IELTS score using Pronunciation , Grammar , Fluency , Vocabulary also  IELTS give scroe like 5,5.5,6,6.5,7,7.5,8,8.5 and 9 and these result prepare from these given questions and answers ${queData} give me score with suggestion separated by :
-
-  //           score should be fix and for each data
-  //           dont give extra information
-  //           it should be same every time and score is calculate by u and calulate all answer  give final answer
-  //           dont give que and answer
-  //           Pronunciation: suggestion with score
-  //           Grammar: suggestion with score
-  //           Fluency: suggestion with score
-  //           Vocabulary: suggestion with score
-  //           Overall score: score`,
-  //         },
-  //       ],
-  //       stream: true,
-  //       max_tokens: 596,
-  //     });
-
-  //     let config = {
-  //       method: 'post',
-  //       maxBodyLength: Infinity,
-  //       url: 'https://api.openai.com/v1/chat/completions',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization:
-  //           'Bearer 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',
-  //       },
-  //       data: data,
-
-  //     };
-
-  //     axios
-  //       .request(config)
-  //       .then(response => {
-  //         console.log('*****response******', response.data);
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-  //       });
-  //   }
-  // }, []);
-
   return (
     <LinearGradient
       colors={[colors.primary, colors.darkPrimary]}
